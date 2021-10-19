f56db0b2cb7005577a7ce07c9f4c06132c7ed0f5
MattBCoding <mattbodden@googlemail.com>
2021-10-19 14:59:38 +0000

    Add codealong challenge part 1

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -3,7 +3,10 @@
  */
 const { tsImportEqualsDeclaration } = require("@babel/types");
 const { beforeAll, test, expect } = require("@jest/globals");
-const { game, newGame, showScore, addTurn, lightsOn, showTurns } = require("../game");
+const { game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn } = require("../game");
+
+jest.spyOn(window, "alert").mockImplementation(() => { });
+
 beforeAll(() => {
     let fs = require("fs");
     let fileContents = fs.readFileSync("index.html", "utf-8");
@@ -86,5 +89,15 @@ describe("gameplay works correctly", () => {
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
+    });
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test("should call an alert if the move is wrong", () => {
+        game.playerMoves.push("wrong");
+        playerTurn();
+        expect(window.alert).toBeCalledWith("Wrong move!");
     });
 });
